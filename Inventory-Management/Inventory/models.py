ea7478651704c1aa825813defd4531e51add13cb
Jose Frade Gala <josefradegala@gmail.com>
2024-07-20 22:12:02 -0400

    Define Stock  model

--- Inventory-Management/Inventory/models.py
+++ Inventory-Management/Inventory/models.py
@@ -3,12 +3,22 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    Category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Stock(models.Model):
+    item = models.ForeignKey('Item', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    # warehouse = models.ForeignKey('Warehouse', on_delete=models.CASCADE) # Uncomment this line when Warehouse model is created
