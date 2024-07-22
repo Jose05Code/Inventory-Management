7e7b6e16b04439bb18a33543333380b031e1cf8c
Jose Frade Gala <josefradegala@gmail.com>
2024-07-22 14:33:56 -0400

    XD

--- Inventory-Management/build_files.sh
+++ Inventory-Management/build_files.sh
@@ -1,3 +1,16 @@
-# build_files.sh
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Install pip if not already installed
+if ! command -v pip &> /dev/null; then
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py --user
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Collect static files
+python manage.py collectstatic --noinput
